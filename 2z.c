a782a3bc356a86bd6763c75d2c6ae89c54cb655a
scrimshandy <synsmtv22@gmail.com>
2025-04-12 12:19:37 +0300

    2z.c

---
+++ 2z.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char colors[3][10] = {"красный", "синий", "зеленый"};
+    strcpy(colors[0], "желтый");
+    for (int i = 0; i < 3; i++) {
+        printf("%s\n", colors[i]);
+    }
+    return 0;
+}
